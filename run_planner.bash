25a32bcef1ecbe1f736a260204330984be83bc3e
Riccardo Lastrico <riky.lastrico@gmail.com>
2021-06-27 18:54:46 +0200

    cleaning and commenting

--- run_planner.bash
+++ run_planner.bash
@@ -3,11 +3,9 @@ path=/mnt/c/Users/rick/Desktop/visits/visits_domain/
 domain=dom1.pddl
 problem=prob1.pddl
 extsolverinput=region_poses
-now="$(date +"%s")"
-#echo $now
 
 popf3path=/mnt/c/Users/rick/Desktop/visits/popf-tif/planner/release/popf/popf3-clp
 visitspath=/mnt/c/Users/rick/Desktop/visits/visits_module/build/libVisits.so
 
-#save plan 
+#run solver
 $popf3path -x $path$domain $path$problem $visitspath $path$extsolverinput 
